ffaa1bd5616b4924a850eabd0ea413349350f266
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-05-09 02:05:50 -0600

    Se creó el componente -Register-.

--- sportsTournamentControl-FrontEnd/src/app/app.module.ts
+++ sportsTournamentControl-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
